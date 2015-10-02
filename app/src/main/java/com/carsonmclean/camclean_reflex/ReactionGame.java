8b454450e26ef2e15306a958402f078c1ab61462
Carson McLean <camclean@ualberta.ca>
2015-10-02 17:54:33 -0600

    Haven't committed in a bit.... whoops

--- app/src/main/java/com/carsonmclean/camclean_reflex/ReactionGame.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/ReactionGame.java
@@ -1,7 +1,42 @@
 package com.carsonmclean.camclean_reflex;
 
+import android.os.Handler;
+import android.os.SystemClock;
+import android.widget.Toast;
+
+import java.util.Random;
+
 /**
  * Created by carsonmclean on 2/10/15.
  */
 public class ReactionGame {
+    public int numberOfPlayers;
+    long startTime = 0;
+    long endTime = 0;
+    long reactionTime;
+
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            startTime = SystemClock.elapsedRealtime();
+            // TODO: Change button color
+        }
+    };
+
+    public void startTimer() {
+//      http://www.mopri.de/2010/timertask-bad-do-it-the-android-way-use-a-handler/
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 1000); // TODO: Change to random time
+    }
+
+    public void buttonClick(int numberOfPlayers, int playerNumber) {
+        endTime = SystemClock.elapsedRealtime();
+        if (startTime == 0) {
+            // Too fast
+        } else {
+            reactionTime = (endTime - startTime);
+            System.out.println(reactionTime);
+        }
+    }
 }
--- app/src/main/java/com/carsonmclean/camclean_reflex/SinglePlayerActivity.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/SinglePlayerActivity.java
@@ -4,33 +4,19 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
-import android.os.CountDownTimer;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import java.util.Random;
-
-// Sources:
-// http://developer.android.com/guide/topics/ui/dialogs.html
 
 public class SinglePlayerActivity extends AppCompatActivity {
-
-    Boolean timerFinished = false; // Has the random wait time ran out yet?
-    long startTime, endTime;
+    ReactionGame reactionGame = new ReactionGame();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_player);
 
-        DialogFragment newFragment = new InformationDialogFragment();
-        // http://stackoverflow.com/questions/8993348/cannot-convert-from-android-support-v4-app-fragment-to-android-app-fragment
-        newFragment.show(getFragmentManager(), "info");
     }
 
     @Override
@@ -55,56 +41,24 @@ public class SinglePlayerActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startSinglePlayer() {
-        // http://stackoverflow.com/questions/1877417/how-to-set-a-timer-in-android
-        Random random = new Random();
-//        new CountDownTimer((long)random.nextInt(1990), 1000) {
-        CountDownTimer countDownTimer = new CountDownTimer(1000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-            @Override
-            public void onFinish() {
-                findViewById(R.id.button5).setBackgroundColor(0xff008000);
-                timerFinished = true;
-                startTime = SystemClock.elapsedRealtime();
-            }
-        };
-        countDownTimer.start();
-
-    }
-
-    public void buttonClick(View view) {
-        //Toast.makeText(SinglePlayerActivity.this, "Click works", Toast.LENGTH_SHORT).show();
-        endTime = SystemClock.elapsedRealtime();
-        if (timerFinished) {
-            long reactionTime = (endTime - startTime);
-            Toast.makeText(SinglePlayerActivity.this,"Your time was " + reactionTime + " milliseconds", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(SinglePlayerActivity.this, "Too fast!\nRestarting timer", Toast.LENGTH_SHORT).show();
-            startSinglePlayer();
-        }
-    }
-
     public class InformationDialogFragment extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle(R.string.single_player_dialog_title);
             builder.setMessage(R.string.single_player_dialog_body);
-            builder.setNegativeButton(R.string.single_player_dialog_go_back, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    // TODO: Go back to main menu
-
-                    // User wants to go back
-                }
-            });
+//            builder.setNegativeButton(R.string.single_player_dialog_go_back, new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int id) {
+//                    // TODO: Go back to main menu
+//
+//                    // User wants to go back
+//                }
+//            });
             builder.setPositiveButton(R.string.single_player_dialog_okay, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
                     // User wants to go play
                     // http://stackoverflow.com/questions/4934333/android-how-to-count-time-over-a-long-period
-                    startSinglePlayer();
+                    reactionGame.startTimer();
                 }
             });
 
