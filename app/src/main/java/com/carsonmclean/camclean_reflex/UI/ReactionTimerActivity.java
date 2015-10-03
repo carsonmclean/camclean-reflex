c766f3be548d0ee20da91af9904cb44139e0bbf8
Carson McLean <camclean@ualberta.ca>
2015-10-03 15:57:44 -0600

    Forgot one settings menu. Cleanded up strings and if statements

--- app/src/main/java/com/carsonmclean/camclean_reflex/UI/ReactionTimerActivity.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/UI/ReactionTimerActivity.java
@@ -48,9 +48,6 @@ public class ReactionTimerActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
