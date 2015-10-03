29c9fc4b440d08e4312fa25c3f6359675941cd5b
Carson McLean <camclean@ualberta.ca>
2015-10-02 22:22:26 -0600

    RTA button onClickListener implemented

--- app/src/main/java/com/carsonmclean/camclean_reflex/UI/ReactionTimerActivity.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/UI/ReactionTimerActivity.java
@@ -6,10 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.carsonmclean.camclean_reflex.R;
+import com.carsonmclean.camclean_reflex.Utilities.ReactionTimerGame;
 
 public class ReactionTimerActivity extends AppCompatActivity {
+    private ReactionTimerGame reactionTimerGame = new ReactionTimerGame(); // TODO: Use the constructor
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,14 @@ public class ReactionTimerActivity extends AppCompatActivity {
         });
         alertDialog.show();
 
-
+        // http://stackoverflow.com/questions/16636752/android-button-onclicklistener
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Got here");
+                reactionTimerGame.onClick();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
@@ -1,7 +1,18 @@
 package com.carsonmclean.camclean_reflex.Utilities;
 
+import android.view.View;
+
 /**
  * Created by carsonmclean on 2/10/15.
  */
 public class ReactionTimerGame {
+
+    // Constructor
+    public ReactionTimerGame() {
+
+    }
+
+    public void onClick() {
+        System.out.println("On click worked!");
+    }
 }
