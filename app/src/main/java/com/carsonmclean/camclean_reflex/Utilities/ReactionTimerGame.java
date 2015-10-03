0d53ce53d0894809db05e591884cd56f463c61af
Carson McLean <camclean@ualberta.ca>
2015-10-02 21:06:32 -0600

    Changed ReactionTimerGame from an Activity to a Class

--- app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
@@ -1,39 +1,7 @@
 package com.carsonmclean.camclean_reflex.Utilities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.carsonmclean.camclean_reflex.R;
-
-public class ReactionTimerGame extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reaction_timer_game);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_reaction_timer_game, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+/**
+ * Created by carsonmclean on 2/10/15.
+ */
+public class ReactionTimerGame {
 }
