add41bfb60ccc451b50b39ebaa88c57112d529c1
Carson McLean <camclean@ualberta.ca>
2015-10-03 14:49:56 -0600

    Timer sort of works?  Crashes b/c timer starts before Dialog dismissed. Rather than dismantling MessagePasser to allow Dialog dismissal, Dylan sugggested starting game by hitting button

--- app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/ReactionTimerGame.java
@@ -3,6 +3,10 @@ package com.carsonmclean.camclean_reflex.Utilities;
 import android.os.SystemClock;
 import android.widget.Button;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by carsonmclean on 2/10/15.
  */
@@ -21,17 +25,32 @@ public class ReactionTimerGame {
 
     public void onClick() {
         reactionTime = (SystemClock.elapsedRealtime() - startTime);
-        buttonColor(button,0xff000000);
+        buttonColor(button, 0xff000000);
         // Want to send message back to Activity, need object to pass message
         messagePasser.createToast("Your reaction time was " + reactionTime + " milliseconds.");
     }
 
     public void startGame() {
-        buttonColor(button,0xffff0000);
-        startTime = SystemClock.elapsedRealtime();
+        buttonColor(button, 0xffff0000);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                buttonColor(button,0xff00ff00);
+                startTime = SystemClock.elapsedRealtime();
+            }
+        };
+        timer.schedule(timerTask, getRandomNumber());
     }
 
-    public void buttonColor(Button button,int color ) {
+    // http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java
+    public long getRandomNumber() {
+        Random random = new Random();
+        return (random.nextInt(2000 - 10) + 10); // TODO: Check this gives proper range
+    }
+
+    public void buttonColor(Button button,int color) {
         button.setBackgroundColor(color);
     }
 }
+
