55de1468e477fb188669f06ad346a8327d49cd0b
Carson McLean <camclean@ualberta.ca>
2015-10-05 02:11:32 -0600

    Statistics went horribly wrong....

--- app/src/main/java/com/carsonmclean/camclean_reflex/UI/StatisticsActivity.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/UI/StatisticsActivity.java
@@ -19,15 +19,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.carsonmclean.camclean_reflex.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class StatisticsActivity extends AppCompatActivity {
+//  CMPUT 301 Fall 2015 lonelyTwitter Lab Code
+    String FILENAME = "reactionTimerGame.sav";
+    private ArrayList<Integer> reactions = new ArrayList<>();
+    private ArrayAdapter<Integer> adapter;
+    private ListView oldReactionsList;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        loadFromFile();
+
+        //  CMPUT 301 Fall 2015 lonelyTwitter Lab Code
+        oldReactionsList = (ListView) findViewById(R.id.oldReactionsList);
+
+
     }
 
     @Override
@@ -50,5 +80,45 @@ public class StatisticsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+//  CMPUT 301 Fall 2015 lonelyTwitter Lab Code
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadFromFile();
+        adapter = new ArrayAdapter<>(this,
+                R.layout.list_item, reactions);
+        oldReactionsList.setAdapter(adapter);
+        adapter.notifyDataSetChanged(); // data has changed, views that use it should refresh
+    }
+
+//  CMPUT 301 Fall 2015 lonelyTwitter Lab Code
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+//          https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html, 2015/09/24
+            Type listType = new TypeToken<ArrayList<Integer>>() {}.getType();
+            reactions = gson.fromJson(in, listType);
+        } catch (FileNotFoundException e) {
+            reactions = new ArrayList<>();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//  CMPUT 301 Fall 2015 lonelyTwitter Lab Code
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, 0);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
