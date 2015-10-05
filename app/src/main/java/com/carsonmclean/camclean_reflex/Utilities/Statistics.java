b682601d67b0c836e3b2e1afde017b63eaec8121
Carson McLean <camclean@ualberta.ca>
2015-10-05 00:01:54 -0600

    Added lab code for loading and saving files

--- app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/Statistics.java
+++ app/src/main/java/com/carsonmclean/camclean_reflex/Utilities/Statistics.java
@@ -15,8 +15,55 @@
 */
 package com.carsonmclean.camclean_reflex.Utilities;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import android.content.Context;
+
 /**
  * Created by carsonmclean on 4/10/15.
  */
 public class Statistics {
+    String FILENAME;
+    ArrayList<Integer> stats;
+
+    // CMPUT 301 Fall 2015 Lab
+    private void loadFromFile(Context context, String FILENAME) {
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+//          https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html, 2015/09/24
+            Type listType = new TypeToken<ArrayList<Integer>>() {
+            }.getType();
+            stats = gson.fromJson(in, listType);
+        } catch (FileNotFoundException e) {
+            stats = new ArrayList<>();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // CMPUT 301 Fall 2015 Lab
+    private void saveInFile(Context context, String FILENAME) {
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, 0);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
